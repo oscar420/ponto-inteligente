7de2dbbd4aee1ee1c0dbbac4ebf9f845650737cd
lKayal <racsonic2@gmail.com>
2021-06-14 11:29:37 -0600

    Adicionando suporte a CI com TravisCI

--- src/main/java/com/kazale/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/kazale/pontointeligente/api/PontoInteligenteApplication.java
@@ -1,4 +1,4 @@
-package com.kazale.pontointeligente.api;
+ 	package com.kazale.pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
