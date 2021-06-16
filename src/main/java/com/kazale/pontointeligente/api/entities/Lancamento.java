c1e776b98e023c8e5d6e42340971cc27c4e6afe2
lKayal <racsonic2@gmail.com>
2021-06-16 17:58:19 -0600

    Commit da primeira migracao criando as tabelas de lancamento, Empresa e Funcionarios, junto com os seus metodos.

--- src/main/java/com/kazale/pontointeligente/api/entities/Lancamento.java
+++ src/main/java/com/kazale/pontointeligente/api/entities/Lancamento.java
@@ -1,5 +1,80 @@
 package com.kazale.pontointeligente.api.entities;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.kazale.pontointeligente.api.enums.TipoEnum;
+import com.sun.istack.NotNull;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Lancamento {
+	
+	private static final long serialVersionUID = 6524560251526772839L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	private Date data;
+	
+	@Column(name = "descricao", nullable = true)
+	private String descricao;
+	
+	@Column(name = "localizacao", nullable = true)
+	private String localizacao;
+	
+	@Column(name = "data_criacao", nullable = false)
+	private Date dataCriacao;
+	
+	@Column(name = "data_atualizacao", nullable = false)
+	private Date dataAtualizacao;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "tipo", nullable = false)
+	private TipoEnum tipo;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	private Funcionario funcionario;
+	
+	@PreUpdate
+    public void preUpdate() {
+        dataAtualizacao = new Date();
+    }
+     
+    @PrePersist
+    public void prePersist() {
+        final Date atual = new Date();
+        dataCriacao = atual;
+        dataAtualizacao = atual;
+    }
+
+	@Override
+	public String toString() {
+		return "Lancamento [id=" + id + ", data=" + data + ", descricao=" + descricao + ", localizacao=" + localizacao
+				+ ", dataCriacao=" + dataCriacao + ", dataAtualizacao=" + dataAtualizacao + ", tipo=" + tipo
+				+ ", funcionario=" + funcionario + "]";
+	}
+
+
 
 }
